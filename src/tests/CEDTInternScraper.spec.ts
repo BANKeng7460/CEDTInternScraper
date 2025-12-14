a71fbc32f7d56deb79c3fa1aa858bcd8a39ff07e
BANKeng7460 <bankeng7460@example.com>
2025-12-14 20:10:52 +0700

    Add competitionRatio , Link

--- src/tests/CEDTInternScraper.spec.ts
+++ src/tests/CEDTInternScraper.spec.ts
@@ -1,28 +1,29 @@
 import { expect } from '@playwright/test';
-import { escapeCsvValue }from '../functions/escapeCsvValue';
+import { escapeCsvValue } from '../functions/escapeCsvValue';
 import * as fs from 'fs';
 import { test } from '../pages/base';
+import { getCurrentDateTimeForFilename } from '../functions/getCurrentDateForFileName';
 
-test('CEDTInternScraper', async ({ page,loginPage }) => {
+test('CEDTInternScraper', async ({ page, loginPage }) => {
   const usernameID = process.env.USERNAMEID!
   const password = process.env.PASSWORD!
   const results = []
-  
-  await loginPage.Login(usernameID,password)
-  
-  await page.getByRole('link', { name: 'ตำแหน่งฝึกงาน', exact:true }).click();
+
+  await loginPage.Login(usernameID, password)
+
+  await page.getByRole('link', { name: 'ตำแหน่งฝึกงาน', exact: true }).click();
   const locatorPosCount = page.locator('p', { hasText: "ผลลัพธ์การค้นหา" })
-                   .locator('span.font-bold')
-                   .first();
+    .locator('span.font-bold')
+    .first();
   await expect(locatorPosCount).not.toHaveText(/0 ตำแหน่งงาน/);
   const positions = await page.locator('p', { hasText: "ผลลัพธ์การค้นหา" }).locator('span.font-bold').first().textContent();
-  
+
   const positionCount = positions == null ? 0 : parseInt(positions.trim(), 10);
-  console.log("position count is",positionCount)
+  console.log("position count is", positionCount)
   for (let i = 0; i < positionCount; i++) {
-    console.log(`Working on ${i+1}/${positionCount}`)
-    const pageNo = Math.ceil((i+1)/20);
-    const cell = (i)%20;
+    console.log(`Working on ${i + 1}/${positionCount}`)
+    const pageNo = Math.ceil((i + 1) / 20);
+    const cell = (i) % 20;
     await page.goto(`https://cedtintern.cp.eng.chula.ac.th/opening?page=${pageNo}&bookmarkedFilter=false&availablePositionFilter=false`)
     const locatorCell = page.locator('.flex.flex-col.w-full.h-fit.rounded-md').nth(cell)
     const jobName = await locatorCell.locator('a').first().textContent()
@@ -30,37 +31,46 @@ test('CEDTInternScraper', async ({ page,loginPage }) => {
     const seatsText = await locatorCell.locator('p', { hasText: 'ที่นั่ง' }).textContent();
     const seats = seatsText?.match(/\d+/)?.[0] ?? null;
     const locator = locatorCell.locator('p', { hasText: 'คนเลือกตำแหน่งนี้' });
-const count = await locator.count();
-
-let selections = 0;  // default 0
-if (count > 0) {
-  const text = await locator.textContent();
-  selections = Number(text?.match(/\d+/)?.[0] ?? 0);
-}
+    const count = await locator.count();
+    
+    let selections = 0;  // default 0
+    if (count > 0) {
+      const text = await locator.textContent();
+      selections = Number(text?.match(/\d+/)?.[0] ?? 0);
+    }
 
     await locatorCell.getByRole('button', { name: 'ดูเพิ่มเติม' }).click()
+    const url = page.url();
+    const hyperlink = `=HYPERLINK("${url}","ดูรายละเอียด")`
     const Compensation = await page.locator('.body1.font-normal.w-\\[70\\%\\].text-high.max-lg\\:w-full').first().textContent()
     const Address = await page.locator('.body1.font-normal.w-\\[70\\%\\].text-high.max-lg\\:w-full').nth(3).textContent()
-    console.log(jobName,companyName,seats,"ที่นั่ง",selections,"คนเลือกตำแหน่งนี้",Compensation,Address)
-    results.push({ "jobPosition":jobName ,"CompanyName":companyName,seats,selections,"Compensation":Compensation,Address});
+
+    let competitionRatio: number | null = null;
+
+    if (seats !== null && Number(seats) > 0) {
+      competitionRatio = selections / Number(seats);
+    }
+
+    console.log(jobName, companyName, seats, "ที่นั่ง", selections, "คนเลือกตำแหน่งนี้", Compensation, Address,competitionRatio, url)
+    results.push({ "jobPosition": jobName, "CompanyName": companyName, seats, selections, "Compensation": Compensation, competitionRatio,url:hyperlink,Address });
   }
 
-  
-
-// สร้าง header
-const header = Object.keys(results[0]).join(',') + '\n';
-
-// สร้าง row แต่ละตัว
-const rows = results.map(obj =>
-  Object.values(obj).map(escapeCsvValue).join(',')
-).join('\n');
-
-// รวม header + rows
-const csvData = header + rows;
 
 
-// เขียนไฟล์ CSV
-  fs.writeFileSync('CEDTInternList.csv', csvData);
+  // สร้าง header
+  const header = Object.keys(results[0]).join(',') + '\n';
+
+  // สร้าง row แต่ละตัว
+  const rows = results.map(obj =>
+    Object.values(obj).map(escapeCsvValue).join(',')
+  ).join('\n');
+
+  // รวม header + rows
+  const csvData = header + rows;
+
+
+  // เขียนไฟล์ CSV
+  fs.writeFileSync(`CEDTInternList${getCurrentDateTimeForFilename()}.csv`, csvData);
 
   console.log('saved → CEDTInternList.csv');
 });
